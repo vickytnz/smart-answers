732b967556accb72a62128279bb86b151041c61d
James Mead <james@floehopper.org>
2016-07-29 15:08:36 +0100

    Introduce DomTestingAssertions module
    This module provides versions of the methods on
    `Rails::Dom::Testing::Assertions` which operate on an HTML string instead of the
    response in a Rails controller test.
    
    * `assert_select` -> `assert_select_for`
    * `css_select` -> `css_select_for`
    
    I plan to use these methods in later commits.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -32,6 +32,7 @@ end
 require 'gds_api/test_helpers/json_client_helper'
 require_relative 'support/fixture_methods'
 require_relative 'support/world_location_stubbing_methods'
+require_relative 'support/dom_testing_assertions'
 
 class ActiveSupport::TestCase
   include FixtureMethods
