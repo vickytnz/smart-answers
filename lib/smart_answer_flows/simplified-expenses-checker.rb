0ed444c2ccb09e3a5400aa9948ce7b77e698aa96
Ikenna N. Okpala <me@ikennaokpala.com>
2017-03-29 12:17:20 +0100

    Remove precalculate block: simplified_expenses_claimed
    This precalculate block was only being used to wrap a calculator
    value and not doing any calculation.
    
    Replaced the simplified_expenses_claimed with the equivalent from
    the calculator
    
    No regression test artefacts have been affected

--- lib/smart_answer_flows/simplified-expenses-checker.rb
+++ lib/smart_answer_flows/simplified-expenses-checker.rb
@@ -258,10 +258,6 @@ module SmartAnswer
           calculator
         end
 
-        precalculate :simplified_expenses_claimed do
-          calculator.simplified_expenses_claimed?
-        end
-
         precalculate :simple_vehicle_costs do
           calculator.simple_vehicle_costs_car_van
         end
